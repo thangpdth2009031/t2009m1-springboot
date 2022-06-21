c44e1399d12138de73eb4792d7741e6d79047332
thangpdth2009031@fpt.edu.vn <thangpdth2009031@fpt.edu.vn>
2022-06-21 09:58:51 +0700

    fix-cors- policy

--- src/main/java/com/example/t2009m1spring/controller/product/ProductController.java
+++ src/main/java/com/example/t2009m1spring/controller/product/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:63343")
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
